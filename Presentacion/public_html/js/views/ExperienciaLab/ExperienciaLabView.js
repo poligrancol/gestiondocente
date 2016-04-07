b33af6ef61e07207d1bfaa99602305170cd1abb1
Jonathan <camillos09@gmail.com>
2016-04-06 22:18:17 -0500

    Creacion del template de Experiencia Docente y modificacion del view.

--- Presentacion/public_html/js/views/ExperienciaLab/ExperienciaLabView.js
+++ Presentacion/public_html/js/views/ExperienciaLab/ExperienciaLabView.js
@@ -2,16 +2,37 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!templates/ExperienciaLab/ExperienciaLaboralTemplate.html'
-], function ($, _, Backbone, ExperienciaLaboralTemplate) {
+    'text!templates/ExperienciaLab/ExperienciaLaboralTemplate.html',
+    'text!templates/ExperienciaLab/MostrarExperienciaRealTemplate.html',
+    'text!templates/ExperienciaLab/MostrarExperienciaDocenteTemplate.html'
+
+], function ($, _, Backbone, ExperienciaLaboralTemplate, MostrarExperienciaRealTemplate, MostrarExperienciaDocenteTemplate) {
 
     var ExperienciaLabView = Backbone.View.extend({
+        el:"#experiencia",
         render: function (accion,id) {
             var data = {};
             var template = _.template(ExperienciaLaboralTemplate);
             var compiledTemplate = template(data);
             $("#experiencia").html(compiledTemplate);  
+        },
+        events: {
+            "click #mostrarExperienciaReal":"mostrarExperienciaReal",
+            "click #mostrarExperienciaDocente":"mostrarExperienciaDocente"
+        },
+        mostrarExperienciaReal: function(){
+            var data = {};
+            var template = _.template(MostrarExperienciaRealTemplate);
+            var compiledTemplate = template(data);
+            $("#experienciacontenido").html(compiledTemplate);  
+        },
+        mostrarExperienciaDocente: function(){
+            var data = {};
+            var template = _.template(MostrarExperienciaDocenteTemplate);
+            var compiledTemplate = template(data);
+            $("#experienciacontenido").html(compiledTemplate);
         }
+
     });
     return ExperienciaLabView;
 });
