3b7dc0697b0983a67f16ed1507527c32a8a2b55d
Jonathan <camillos09@gmail.com>
2016-04-11 19:45:46 -0500

    Se modifico los templates de Mostrar Experiencia Real, Mostrar Experiencia Docente y Agregar Experiencias.

--- Presentacion/public_html/js/modules/docente/main.js
+++ Presentacion/public_html/js/modules/docente/main.js
@@ -9,8 +9,8 @@ require.config({
     'bootstrap' : { 'deps' :['jquery'] }
   },
   paths: {
-    bootstrap: 'libs/lesson/bootstrap.min.js',
     jquery: 'libs/jquery/jquery-2.1.3.min',
+    bootstrap: 'libs/bootstrap/bootstrap.min',
     underscore: 'libs/underscore/underscore-min',
     backbone: 'libs/backbone/backbone-min',
     templates: '../templates'
@@ -20,9 +20,10 @@ require.config({
 
 require([
   // Load our app module and pass it to our definition function
+  'jquery',
   'modules/docente/app'
 
-], function(App){
+], function($,App){
   // The "app" dependency is passed in as "App"
   // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
   App.initialize();
