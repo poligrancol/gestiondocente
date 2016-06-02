62955a117992c4b534503b71051453e21e611503
Miller Carreño <macarrenob@poligran.edu.co>
2016-06-02 14:36:04 -0500

    Escolaridad View

--- Presentacion/public_html/js/views/Escolaridad/EscolaridadView.js
+++ Presentacion/public_html/js/views/Escolaridad/EscolaridadView.js
@@ -6,9 +6,20 @@ define([
     'models/EscolaridadModel',
     'collections/EscolaridadCollection',
     'text!templates/Escolaridad/ListarEscolaridadTemplate.html',
-    'text!templates/Escolaridad/EditarEscolaridadTemplate.html',
-], function ($, _, Backbone, b, EscolaridadModel, EscolaridadCollection, ListarEscolaridadTemplate, EditarEscolaridadTemplate) {
+      'text!templates/Escolaridad/EditarEscolaridadTemplate.html',
+        //'text!templates/Escolaridad/ListarTecnicosTemplate.html',
+        'text!templates/Escolaridad/ListarTecnologoTemplate.html'
+        //'text!templates/Escolaridad/ListarProfesionalTemplate.html',
+        //'text!templates/Escolaridad/ListarEspecialTemplate.html',
+        //'text!templates/Escolaridad/ListarMaestriaTemplate.html',
+        //'text!templates/Escolaridad/ListarDoctoradoTemplate.html'
 
+], function ($, _, Backbone, b, EscolaridadModel, EscolaridadCollection, ListarEscolaridadTemplate, 
+            EditarEscolaridadTemplate, ListarTecnologoTemplate) {
+
+            //url: '/EscolaridadCollection '
+            //DEBO USAR : "EscolaridadCollection" acá??
+            
     var EscolaridadView = Backbone.View.extend({
         className: "escolaridad-section",
         //el:"#escolaridad",  NUEVA - SE PUEDE DE LAS DOS FORMAS??
@@ -16,17 +27,37 @@ define([
         model: new EscolaridadModel(),
         collection: new EscolaridadCollection(),
         render: function (idDocente) {
-            if (idDocente) {
-                //this.model.set({idProfesor: id});
+                 if (idDocente) {
+               
+                this.model.set({idProfesor: idDocente});
+                var that = this;
+                this.collection.fetch({
+                       success:function(escolaridades){
+                           var arra = [];
+                        _.each(escolaridades, function (usuario){
+                            var template = _.template(ListarTecnologoTemplate, {model:usuario});
+                            
+                            arra.push(template);
+                            //that.$el.html({model:escolaridades.ListarTecnologoTemplate});
+
+                        }) 
+                           
+                           
+                        var  comp = _.template(ListarEscolaridadTemplate);
+                        //that.$el.html(comp({usuarios:'PRUEBA'}));
+                        that.$el.html(comp({escolaridades:arra}));
+                       } 
+                    }); 
             }
             var options = {
                 success: this.cargarDatos,
                 error: this.errorConsulta
             };
-                //  $("#escolaridad").html(compiledTemplate);
-                //this.model.sync("read",this.model,options);
-           // this.model.set({codigoEscolaridad:1});
-           //this.model.fetch();
+            var  compiledTemplate = _.template(ListarEscolaridadTemplate);
+                  $("#escolaridad").html(compiledTemplate);
+                this.model.sync("read",this.model,options);
+            this.model.set({codigoEscolaridad:1});
+            this.model.fetch();
             this.cargarDatos(this.model);
         },
         events: {
@@ -59,7 +90,7 @@ define([
         editarEscolaridad : function(){
             console.log('cargar editar datos');
             this.cargarEditar();
-        },
+        }, 
     });
     return EscolaridadView;
 });
